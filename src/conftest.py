8f9bc6bcd7cfc5951c88c6f75e0c3b9521aab125
Dawid Jagoda <djago@softserveinc.com>
2021-10-08 14:58:34 +0200

    Directories and Objects

--- src/conftest.py
+++ src/conftest.py
@@ -1,7 +1,17 @@
 @pytest.fixture
-def driver():
+def driver(auth, download_dir):
     # Initialize ChromeDriver
-    driver = Driver()
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument("--window-size=1920, 1080")
+    chrome_options.add_argument('--headless')
+    prefs = {'download.default_directory': download_dir}
+    chrome_options.add_experimental_option('prefs', prefs)
+    driver = Chrome(options=chrome_options)
+    # add auth session to drivers attrs
+    driver.auth_session = auth
+    # Return the driver object at the end of setup
     yield driver
     # For cleanup, quit the driver
     driver.quit()
